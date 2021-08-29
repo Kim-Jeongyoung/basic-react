495145925a857b6597275c457b4a4cd4643772d6
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-08-29 11:29:38 -0700

    Style the completed todo items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React from 'react';
+// import React from 'react';
 
+// import TodoItem from './TodoItem';
+// import todosData from './todosData';
+
+// //class-based component
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       todos: todosData,
+//     };
+//   }
+
+//   // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+//   // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+//   handleChange(id) {
+//     this.setState((prevState) => {
+//       const updatedTodos = prevState.todos.map((todo) => {
+//         if (todo.id === id) {
+//           //modify prevState: Don't do that
+//           // todo.completed = !todo.completed;
+//           //returning new object
+//           return {
+//             ...todo,
+//             completed: !todo.completed,
+//           };
+//         }
+//         return todo;
+//       });
+//       return {
+//         todos: updatedTodos,
+//       };
+//     });
+//   }
+
+//   render() {
+//     const todoItems = this.state.todos.map((item) => (
+//       <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+//     ));
+
+//     return <div className="todo-list">{todoItems}</div>;
+//   }
+// }
+
+// export default App;
+
+// // const App = () => {
+// //   const todosDataComponent = todosData.map((item) => (
+// //     <TodoItem key={item.id} item={item} />
+// //   ));
+// //   return <div className="todo-list">{todosDataComponent}</div>;
+// // };
+
+// // export default App;
+
+import React from 'react';
 import TodoItem from './TodoItem';
 import todosData from './todosData';
-
-//class-based component
 
 class App extends React.Component {
   constructor() {
@@ -11,17 +65,14 @@ class App extends React.Component {
     this.state = {
       todos: todosData,
     };
+    // bind를 해 줘야 함
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-  // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
   handleChange(id) {
     this.setState((prevState) => {
       const updatedTodos = prevState.todos.map((todo) => {
         if (todo.id === id) {
-          //modify prevState: Don't do that
-          // todo.completed = !todo.completed;
-          //returning new object
           return {
             ...todo,
             completed: !todo.completed,
@@ -37,7 +88,7 @@ class App extends React.Component {
 
   render() {
     const todoItems = this.state.todos.map((item) => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
 
     return <div className="todo-list">{todoItems}</div>;
@@ -45,11 +96,3 @@ class App extends React.Component {
 }
 
 export default App;
-// const App = () => {
-//   const todosDataComponent = todosData.map((item) => (
-//     <TodoItem key={item.id} item={item} />
-//   ));
-//   return <div className="todo-list">{todosDataComponent}</div>;
-// };
-
-// export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const TodoItem = (props) => {
+function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#cdcdcd',
+    textDecoration: 'line-through',
+  };
+
   return (
     <div className="todo-item">
       <input
         type="checkbox"
-        checked={props.item.complete}
+        checked={props.item.completed}
         onChange={() => props.handleChange(props.item.id)}
       />
-      <p>{props.item.text}</p>
+      <p style={props.item.completed ? completedStyle : null}>
+        {props.item.text}
+      </p>
     </div>
   );
-};
+}
 
 export default TodoItem;
